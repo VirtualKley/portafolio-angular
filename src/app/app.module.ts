9fb6a4b62856d3e55fa98a7a230eed4c2d0d6f20
VirtualKley <javolkley@gmail.com>
2021-09-05 23:58:21 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { SocialBarComponent } from './components/shared/social-bar/social-bar.component';
+import { SkillComponent } from './components/skill/skill.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    SocialBarComponent,
+    SkillComponent
   ],
   imports: [
     BrowserModule,
